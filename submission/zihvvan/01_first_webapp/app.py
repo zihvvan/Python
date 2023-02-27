da84d60b426f742a4c51f70be67f54548e449d8d
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-02-27 17:40:41 +0900

    streamlit

---
+++ submission/zihvvan/01_first_webapp/app.py
@@ -0,0 +1,17 @@
+# streamlit 라이브러리 호출
+import streamlit as st
+
+# 마크다운을 기반으로 한 꾸미기 기능 작동
+st.write(
+    """
+    # 나에게 가장 도움이 될 것 같은 유튜브
+    ## 나도코딩
+    >이유 : 빠르게 넘겨 볼 수 있는 코딩 교육 자료들이 많다.
+    [링크](https://www.youtube.com/@nadocoding/playlists)
+
+    # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
+    ## 프로그래머 김플 스튜디오
+    > 이유 :  ChatGPT, IT NEWS 등 최신 소식들을 잘 알려준다.
+    [링크](https://www.youtube.com/@kimfl)
+    """
+)
