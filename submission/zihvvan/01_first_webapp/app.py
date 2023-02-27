4bbdcc7f127d437794998d4264f890f1cc67fdf0
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-02-27 18:18:38 +0900

    final

--- submission/zihvvan/01_first_webapp/app.py
+++ submission/zihvvan/01_first_webapp/app.py
@@ -4,15 +4,17 @@ import streamlit as st
 # 마크다운을 기반으로 한 꾸미기 기능 작동
 st.write(
     """
-    # 나에게 가장 도움이 될 것 같은 유튜브
+    # 나에게 가장 도움이 될 것 같은
+    # 유튜브
     ## 나도코딩
     >이유 : 빠르게 넘겨 볼 수 있는 코딩 교육 자료들이 많다.
-    [유튜브 바로가기](https://www.youtube.com/@nadocoding/playlists)
+    <mark>[유튜브 바로가기](https://www.youtube.com/@nadocoding/playlists)</mark>
 
-    # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
+    # 다른 학생들에게 가장 도움이 
+    # 될 것 같은 유튜브
     ## 프로그래머 김플 스튜디오
     > 이유 :  ChatGPT, IT NEWS 등 최신 소식들을 잘 알려준다.
-    [유튜브 바로가기](https://www.youtube.com/@kimfl)
+    <mark>[유튜브 바로가기](https://www.youtube.com/@kimfl)</mark>
 
     # 참고 사이트
     * [icons8](https://icons8.com/)
