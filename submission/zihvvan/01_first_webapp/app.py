8428e9faec3bd7e8b061dde72a8adaa259ea9b96
zihvvan <105600496+zihvvan@users.noreply.github.com>
2023-02-27 18:11:28 +0900

    final

--- submission/zihvvan/01_first_webapp/app.py
+++ submission/zihvvan/01_first_webapp/app.py
@@ -7,12 +7,12 @@ st.write(
     # 나에게 가장 도움이 될 것 같은 유튜브
     ## 나도코딩
     >이유 : 빠르게 넘겨 볼 수 있는 코딩 교육 자료들이 많다.
-    [링크](https://www.youtube.com/@nadocoding/playlists)
+    [유튜브 바로가기](https://www.youtube.com/@nadocoding/playlists)
 
     # 다른 학생들에게 가장 도움이 될 것 같은 유튜브
     ## 프로그래머 김플 스튜디오
     > 이유 :  ChatGPT, IT NEWS 등 최신 소식들을 잘 알려준다.
-    [링크](https://www.youtube.com/@kimfl)
+    [유튜브 바로가기](https://www.youtube.com/@kimfl)
 
     # 참고 사이트
     * [icons8](https://icons8.com/)
@@ -22,3 +22,6 @@ st.write(
 
     
 )
+
+# """ """ : 여러 줄을 묶어서 표시할 수 있는 문자열
+# ''' ''' (동일기능)
